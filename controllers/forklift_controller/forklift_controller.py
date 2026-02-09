e3829bf0d2eef9320eab8cef9a5f92ddd5d3d52f
KarlBorromeo <lrakborromeo@gmail.com>
2026-02-09 11:44:33 +0800

    Refactor forklift controller and update proto definitions for improved motor control and structure

--- controllers/forklift_controller/forklift_controller.py
+++ controllers/forklift_controller/forklift_controller.py
@@ -9,36 +9,35 @@ keyboard = Keyboard()
 keyboard.enable(timestep)
 
 # Speed (same idea as C code)
-speed = 2.0
+speed = -2.0
 
 # Get motors (names MUST match your robot)
 front_left_motor = robot.getDevice("front_left_motor")
 front_right_motor = robot.getDevice("front_right_motor")
-rear_left_motor = robot.getDevice("rear_left_motor")
-rear_right_motor = robot.getDevice("rear_right_motor")
 slider_motor = robot.getDevice("slider_motor")
-front_left_steer_motor = robot.getDevice("front_left_steer_motor")
-front_right_steer_motor = robot.getDevice("front_right_steer_motor")
+rear_left_steer_motor = robot.getDevice("rear_left_steer_motor")
+rear_right_steer_motor = robot.getDevice("rear_right_steer_motor")
 
 
 # Velocity control mode
+# front_left_motor.setPosition(float('inf'))
+# front_right_motor.setPosition(float('inf'))
 front_left_motor.setPosition(float('inf'))
 front_right_motor.setPosition(float('inf'))
-rear_left_motor.setPosition(float('inf'))
-rear_right_motor.setPosition(float('inf'))
 slider_motor.setPosition(float('inf'))
-front_left_steer_motor.setPosition(float('inf'))
-front_right_steer_motor.setPosition(float('inf'))
+rear_left_steer_motor.setPosition(float('inf'))
+rear_right_steer_motor.setPosition(float('inf'))
 
+# front_left_motor.setVelocity(speed)
+# front_right_motor.setVelocity(speed)
 front_left_motor.setVelocity(speed)
 front_right_motor.setVelocity(speed)
-rear_left_motor.setVelocity(speed)
-rear_right_motor.setVelocity(speed)
 slider_motor.setVelocity(0.0)
-front_left_steer_motor.setVelocity(speed)
-front_right_steer_motor.setVelocity(speed)
-front_left_steer_motor.setPosition(0.0)
-front_right_steer_motor.setPosition(0.0)
+slider_motor.setPosition(0.001)
+rear_left_steer_motor.setVelocity(speed)
+rear_right_steer_motor.setVelocity(speed)
+rear_left_steer_motor.setPosition(0.0)
+rear_right_steer_motor.setPosition(0.0)
 
 print("Select the 3D window and use the keyboard:")
 print(" W: forward")
@@ -54,23 +53,19 @@ while robot.step(timestep) != -1:
     if key == ord('W'):
         front_left_motor.setVelocity(speed)
         front_right_motor.setVelocity(speed)
-        rear_left_motor.setVelocity(speed)
-        rear_right_motor.setVelocity(speed)
 
 
     elif key == ord('A'):
-        front_left_steer_motor.setPosition(1.0)
-        front_right_steer_motor.setPosition(1.0)
+        rear_left_steer_motor.setPosition(-1.0)
+        rear_right_steer_motor.setPosition(-1.0)
 
     elif key == ord('S'):
         front_left_motor.setVelocity(-speed)
-        rear_left_motor.setVelocity(-speed)
         front_right_motor.setVelocity(-speed)
-        rear_right_motor.setVelocity(-speed)
 
     elif key == ord('D'):
-        front_left_steer_motor.setPosition(-1.0)
-        front_right_steer_motor.setPosition(-1.0)
+        rear_left_steer_motor.setPosition(1.0)
+        rear_right_steer_motor.setPosition(1.0)
 
     elif key == ord('Q'):
         slider_motor.setVelocity(0.2)
@@ -78,12 +73,10 @@ while robot.step(timestep) != -1:
 
     elif key == ord('E'):
         slider_motor.setVelocity(0.2)
-        slider_motor.setPosition(0.0)
+        slider_motor.setPosition(0.001)
 
     elif key == ord('X'):
         front_left_motor.setVelocity(0.0)
         front_right_motor.setVelocity(0.0)
-        rear_left_motor.setVelocity(0.0)
-        rear_right_motor.setVelocity(0.0)
-        front_left_steer_motor.setPosition(0.0)
-        front_right_steer_motor.setPosition(0.0)
+        rear_left_steer_motor.setPosition(0.0)
+        rear_right_steer_motor.setPosition(0.0)
