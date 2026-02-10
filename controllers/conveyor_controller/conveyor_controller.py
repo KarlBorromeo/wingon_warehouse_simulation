2397773a656fa6b2c9f5580124ee51e1b567acec
KingsDevs <sunnyckair@gmail.com>
2026-02-10 12:50:38 +0800

    Refactor conveyor controller to use Supervisor and add box spawner functionality; update world file with new box spawner definition

--- controllers/conveyor_controller/conveyor_controller.py
+++ controllers/conveyor_controller/conveyor_controller.py
@@ -1,22 +1,41 @@
-from controller import Robot, Motor
+from controller import Supervisor, Motor
 
-# create the Robot instance.
-robot = Robot()
+robot = Supervisor()
 timestep = int(robot.getBasicTimeStep())
 
 belt_motor: Motor = robot.getDevice("belt_motor")
-belt_motor.setPosition(float('inf'))  # Set to velocity control mode
-belt_motor.setVelocity(0.0)  # Start with the belt stopped
+belt_motor.setPosition(float('inf'))
+belt_motor.setVelocity(0.0)
+
+# Get box spawner position
+box_spawner = robot.getFromDef("box_spawner")
+box_spawner_tf = box_spawner.getField("translation")
+
+# Timing
+spawn_interval = 5.0  # seconds
+last_spawn_time = 0.0
+sim_time = 0.0
 
 while robot.step(timestep) != -1:
-    belt_motor.setVelocity(0.3)  # Set belt speed to 2.0 (adjust as needed)
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+    belt_motor.setVelocity(0.3)
 
-    # Process sensor data here.
+    sim_time += timestep / 1000.0
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
+    if sim_time - last_spawn_time >= spawn_interval:
+        last_spawn_time = sim_time
 
-# Enter here exit cleanup code.
+        x, y, z = box_spawner_tf.getSFVec3f()
+        # z += 0.02  # small lift to avoid collision with belt
+
+        box_proto = f"""
+        CardboardBox {{
+          translation {x} {y} {z}
+          rotation -0.3016120512105832 -0.9349314530822059 -0.18690465109819893 2.593254445485388e-06
+          size 0.31 0.37 0.24
+          mass 1
+        }}
+        """
+
+        robot.getRoot().getField("children").importMFNodeFromString(
+            -1, box_proto
+        )
